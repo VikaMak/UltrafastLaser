44f7fcd99d70c5c792ba5db68d708c6c6068c82e
VikaMak <viktoryia.mak@gmail.com>
2016-05-29 13:12:19 +0200

    Added Ajax class

--- app/models/ajax.php
+++ app/models/ajax.php
@@ -1,34 +1,42 @@
 <?php
 require_once '../core/DataBase.php';
+require_once '../core/Check.php';
 
-	function check_data_in($data) {
-		$data = trim($data);
-		$data = stripslashes($data);
-		$data = htmlspecialchars($data, ENT_QUOTES, 'UTF-8');
-		return $data;
-	}
+class Ajax {
 	
-	$array = [];
-	
-	$db = new DataBase();
-	
-	if (isset($_POST['name'], $_POST['comment'])) {
-	
-		$id = intval($_POST['id']);
-		$name = check_data_in($_POST['name']);
-		$comment = check_data_in($_POST['comment']);
+	public $array = [];
+	protected $id;
+	protected $name;
+	protected $comment;
 		
-		$result1 = $db->mysqli->prepare("INSERT INTO `comments` (`entry_id`, `name`, `text`, `date`)
-												VALUES (?,?,?,NOW())");
-		$result1->bind_param('iss',$id, $name, $comment);
-		$result1->execute();
-		$result1->close();
-		
-		$array = [	
-				'name' => $name,
-				'comment' => $comment,
-		];
-		echo json_encode($array);
-	}
+	use Check;
 	
-	$db->mysqli->close();
+	public function __construct() {
+		
+		$db = new DataBase();
+	
+		if (isset($_POST['name'], $_POST['comment'])) {
+		
+			$this->id = intval($_POST['id']);
+			$this->name = $this->check_data_in($_POST['name']);
+			$this->comment = $this->check_data_in($_POST['comment']);
+			
+			$result1 = $db->mysqli->prepare("INSERT INTO `comments` (`entry_id`, `name`, `text`, `date`)
+											VALUES (?,?,?,NOW())");
+			$result1->bind_param('iss',$this->id, $this->name, $this->comment);
+			$result1->execute();
+			$result1->close();
+			
+			$this->array = [	
+					'name'    => $this->name,
+					'comment' => $this->comment,
+			];
+			echo json_encode($this->array);
+		}
+		
+		$db->mysqli->close();
+		}
+	
+}
+	
+$ajax = new Ajax();
