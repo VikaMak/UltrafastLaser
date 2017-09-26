fe70021d165a40d78926a26f3cde86dc543dd0b5
VikaMak <viktoryia.mak@gmail.com>
2017-09-26 07:33:19 +0300

    Some corrections

--- app/init.php
+++ app/init.php
@@ -1,4 +1,14 @@
 <?php
+
+$GLOBALS['config'] = [
+    'mysql' => [
+        'host' => 'localhost',
+        'user' => 'root',
+        'password' => '',
+        'name' => 'main1'
+    ]
+];
+
 spl_autoload_register(function($class) {
 	require_once "core/{$class}.php";
 });
