8cc5e79ed481fc814f4e53cf6ce6eb828cd21758
VikaMak <viktoryia.mak@gmail.com>
2017-09-26 07:20:56 +0300

    Some corrections

--- app/models/ajax.php
+++ app/models/ajax.php
@@ -1,42 +1,42 @@
 <?php
-require_once '../core/DataBase.php';
-require_once '../core/Check.php';
+
+require_once '../init.php';
 
 class Ajax {
-	
-	public $array = [];
-	protected $id;
-	protected $name;
-	protected $comment;
-		
-	use Check;
-	
-	public function __construct() {
-		
-		$db = new DataBase();
-	
-		if (isset($_POST['name'], $_POST['comment'])) {
-		
-			$this->id = intval($_POST['id']);
-			$this->name = $this->check_data_in($_POST['name']);
-			$this->comment = $this->check_data_in($_POST['comment']);
-			
-			$result1 = $db->mysqli->prepare("INSERT INTO `comments` (`entry_id`, `name`, `text`, `date`)
-											VALUES (?,?,?,NOW())");
-			$result1->bind_param('iss',$this->id, $this->name, $this->comment);
-			$result1->execute();
-			$result1->close();
-			
-			$this->array = [	
-					'name'    => $this->name,
-					'comment' => $this->comment,
-			];
-			echo json_encode($this->array);
-		}
-		
-		$db->mysqli->close();
-		}
-	
+    
+    public $ajaxResult = [];
+    private $id;
+    private $name;
+    private $comment;
+    private $db;
+        
+    use Check;
+    
+    public function __construct()
+    {
+        $this->db = DataBase::getInstance();
+    
+        if (isset($_POST['name'], $_POST['comment'])) {
+        
+            $this->id = intval($_POST['id']);
+            $this->name = $this->check_data_in($_POST['name']);
+            $this->comment = $this->check_data_in($_POST['comment']);
+            
+            $this->db->query("INSERT INTO comments (entry_id, name, text, date)
+                                  VALUES (?,?,?,NOW())", [
+                                  $this->id, $this->name, $this->comment])->results();
+            
+            $this->ajaxResult = [                     
+                    'name'    => $this->name,
+                    'comment' => $this->comment,
+            ];
+            
+            echo json_encode($this->ajaxResult);
+            
+        }
+        
+    }
+    
 }
-	
+    
 $ajax = new Ajax();
